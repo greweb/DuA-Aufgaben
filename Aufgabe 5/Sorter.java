dfc71279fbf781567c89c9d705560cee7b9daf64
AntonvomBerg <stehle.adrian@gmail.com>
2022-05-09 17:32:50 +0200

    minor changes

--- Aufgabe 5/Sorter.java
+++ Aufgabe 5/Sorter.java
@@ -1,24 +1,77 @@
 public class Sorter 
 {
-    int numbers[]={0,0,0,0,0,0,0,0};
-    public int Mergesort(int numbers[])
+    public int[] mergesort(int numbers[])
     {
-        numbers = this.numbers;
-        int partOne[]={};
-        int partTwo[]={};
+        
+        int [] partOne = new int[numbers.length/2];
+
+        int [] arrayback = new int[numbers.length];
+        int [] partTwo;
+        if (numbers.length%2 == 1) {
+            partTwo = new int[(numbers.length/2)+1];
+        }
+        else {
+            partTwo = new int[(numbers.length/2)];
+        }
         int j=0;
-        for(int i=0;i<=numbers.length;i++)
+        for(int i=0;i<numbers.length;i++)
         {
             if(i<numbers.length/2)
             {
                 partOne[i]=numbers[i];
             }
+            
             else
             {
                 partTwo[j]=numbers[i];
                 j++;
             }
         }
-        Mergesort();
+        if (partOne.length>=2){
+        	partOne = mergesort(partOne);
+        }
+        if(partTwo.length>=2){
+        	partTwo = mergesort(partTwo);
+        } 	
+        
+        	//merge
+    	int x = 0;
+    	int y = 0;
+    	int z = 0;
+    	while (x+y != numbers.length  ) {
+
+        	System.out.print(x+" "+y+" "+z+"     ");
+    		if (x>(partTwo.length)-1) {
+    			arrayback[z] = partOne[y];
+    			y++;
+    		}
+    		else if (y>(partOne.length)-1) {
+    			arrayback[z] = partTwo[x];
+    			x++;
+    		}
+    		else if (partTwo[x] > partOne[y]) {
+    			arrayback[z] = partOne[y];
+    			y++;
+    		}
+    		else {
+    			arrayback[z] = partTwo[x];
+    			x++;
+    		}
+    		z++;
+
+        	System.out.println(x+" "+y+" "+z+"     ");
+	
+        }
+    	System.out.println(arrayback[0]+" ");	
+    	
+		return arrayback;
+        
+        
+        
+        
+        
+        
+        
+        
     }    
 }
--- Aufgabe 5/main.java
+++
@@ -1,9 +0,0 @@
-public class main 
-{
-    public static void main(String[] args) 
-    {
-        Sorter mergesort= new Sorter();
-        int a[] = {3,63,456,346,3456,346,346,34};
-        mergesort.Mergesort(a);
-    }
-}
